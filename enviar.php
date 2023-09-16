99d6719ee96a3e309a010a8b7f1aaabdc2cc381a
luciano771 <79546157+luciano771@users.noreply.github.com>
2023-09-16 06:51:02 -0300

    se agrego formulario

---
+++ enviar.php
@@ -0,0 +1,40 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'pereyraluciano771@gmail.com'; // Cambia esto a tu dirección de correo Gmail
+        $mail->Password = '422691431'; // Cambia esto a tu contraseña de correo Gmail
+        $mail->SMTPSecure = 'tls'; // Puede ser 'ssl' o 'tls'
+        $mail->Port = 587; // Puerto SMTP
+
+        $mail->setFrom($email, $nombre);
+        $mail->addAddress('pereyraluciano771@gmail.com'); // Cambia esto a la dirección de destino
+        $mail->isHTML(false);
+
+        $mail->Subject = "Mensaje de contacto de $nombre";
+        $mail->Body = "Nombre: $nombre\nEmail: $email\nMensaje:\n$mensaje";
+
+        $mail->send();
+        echo "Gracias por contactarnos. Su mensaje ha sido enviado.";
+    } catch (Exception $e) {
+        echo  $e;
+    }
+} else {
+    echo "Acceso no permitido.";
+}
+?>
