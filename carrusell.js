92969c0a2ae2546312e45707aaf1eb63ba30398e
luciano771 <79546157+luciano771@users.noreply.github.com>
2023-09-17 20:59:50 -0300

    meraki 1.0.0.0

--- carrusell.js
+++ carrusell.js
@@ -1,10 +1,9 @@
-
 $(document).ready(function(){
     $('.project-carousel').slick({
         slidesToShow: 3, // Número de proyectos a mostrar en un momento dado
         slidesToScroll: 1, // Número de proyectos a desplazarse a la vez
         autoplay: true, // Activar reproducción automática
-        autoplaySpeed: 1200, // Duración de cada diapositiva en milisegundos
+        autoplaySpeed: 900, // Duración de cada diapositiva en milisegundos
         responsive: [
             {
                 breakpoint: 768, // Cambia el número de proyectos a mostrar en resoluciones más pequeñas
